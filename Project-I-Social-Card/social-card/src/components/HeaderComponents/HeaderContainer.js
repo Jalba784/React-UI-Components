f4fd00c59655d0f9b089f105d3d4508d609458cb
josepaniagua <jpanjr@gmail.com>
2019-05-30 15:54:26 -0700

    header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from 'ImageThumbnail';
+import HeaderTitle from 'HeaderTitle';
+import HeaderContent from 'HeaderContent';
+
+function HeaderContainer() {
+    return (
+        <div className={'header-container'}>
+            <div className={'img-cont'}>
+                <ImageThumbnail/>
+            </div>
+            <div className={'header-main-cont'}>
+                <HeaderTitle/>
+                <HeaderContent/>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    var options = {month: 'short', day: '2-digit'};
+    let timestamp = new Intl.DateTimeFormat('en-US', options).format(date);
+
+    return (
+        <h1>Lambda School <span className={'cyberLambda'}>@LambdaSchool</span><span className={'timeStamp'}>{timestamp}</span></h1>
+    )
+}
+
+export default HeaderTitle;
