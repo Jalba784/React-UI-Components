774c5e4d52932df72b0e1127b2b3f0f0891d489d
josepaniagua <jpanjr@gmail.com>
2019-06-02 01:12:45 -0700

    trying to add icons to footer

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-icons'>
+            <i className="fas fa-comment" id='hg'></i>
+        </div>
+    );
+}
+
+export default Footer;
