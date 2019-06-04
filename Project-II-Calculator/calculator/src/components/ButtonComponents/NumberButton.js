d7f2aacb6ef6dc08d1b2b5d2cc63029331d4ad67
josepaniagua <jpanjr@gmail.com>
2019-06-03 23:45:20 -0700

    NumberButton component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Button.css';
 
-const NumberButton = () => {
-  return ;
+const NumberButton = (props) => {
+  return (
+      <button className={props.buttonStyle}>{props.text}</button>
+  );
 };
 
 export default NumberButton
